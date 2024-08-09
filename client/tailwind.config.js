ae36f0ac3b5c3f853705d84eb2093f2cf3f64af4
alpturan-dev <abdurrahmanalpturan13@gmail.com>
2024-08-09 17:57:23 +0300

    fix shadcnui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     './app/**/*.{js,jsx}',
     './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -18,12 +19,12 @@ module.exports = {
     extend: {
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
