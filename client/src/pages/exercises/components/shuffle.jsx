65242b970c4eed1310080a4e063177cbe4efe7c2
alpturan-dev <abdurrahmanalpturan13@gmail.com>
2024-09-10 21:34:46 +0300

    fix shuffle game

--- client/src/pages/exercises/components/shuffle.jsx
+++ client/src/pages/exercises/components/shuffle.jsx
@@ -15,6 +15,7 @@ const Shuffle = () => {
     const [isCorrect, setIsCorrect] = useState(null);
     const [score, setScore] = useState(0);
     const [showTryAgain, setShowTryAgain] = useState(false);
+    const [usedWords, setUsedWords] = useState([]);
 
     const getAllWordsByCollection = async () => {
         try {
@@ -42,9 +43,16 @@ const Shuffle = () => {
     };
 
     const nextWord = (wordsArray) => {
-        const shuffledWords = shuffleArray(wordsArray || words);
+        const remainingWords = wordsArray.filter(word => !usedWords.includes(word.targetWord));
+
+        if (remainingWords.length === 0) {
+            resetGame();
+            return;
+        }
+        const shuffledWords = shuffleArray(remainingWords);
         const newCurrentWord = shuffledWords[0];
         setCurrentWord(newCurrentWord);
+        setUsedWords(prev => [...prev, newCurrentWord.targetWord]);
 
         const incorrectOptions = shuffleArray(shuffledWords.filter(word => word !== newCurrentWord))
             .slice(0, 3)
@@ -62,7 +70,7 @@ const Shuffle = () => {
         if (selectedOption === currentWord.targetWord) {
             setIsCorrect(true);
             setScore(score + 1);
-            setTimeout(() => nextWord(), 1000);
+            setTimeout(() => nextWord(words), 700);
         } else {
             setIsCorrect(false);
             setShowTryAgain(true);
@@ -72,6 +80,16 @@ const Shuffle = () => {
     const handleTryAgain = () => {
         setIsCorrect(null);
         setShowTryAgain(false);
+        nextWord(words);
+        setScore(0);
+    };
+
+    const resetGame = () => {
+        setUsedWords([]);
+        setScore(0);
+        setShowTryAgain(false);
+        setIsCorrect(null);
+        nextWord(words);
     };
 
     if (!currentWord) {
@@ -83,13 +101,24 @@ const Shuffle = () => {
     }
 
     return (
-        <main className="flex-1 container px-4 md:px-62">
-            <div className="max-w-2xl mx-auto p-4 text-center">
-                <div className="py-4 flex items-center gap-2 text-2xl font-semibold underline underline-offset-2">
+        <main className="bg-background text-foreground py-4">
+            <div className="mx-auto text-center">
+                <div className="py-4 flex items-center gap-2 text-2xl font-semibold">
                     <span>{t('shuffleGame.title')}</span>
                 </div>
-                <h2 className="text-2xl font-bold mb-4">{t('shuffleGame.score')} {score}</h2>
                 <hr className="shadow-xl" />
+                <div className="pt-6 w-full flex justify-center">
+                    <div className="relative w-1/2 sm:w-1/4">
+                        <h2 className="rounded-lg py-2 px-4 text-2xl font-bold bg-gray-700 text-white">
+                            {t('shuffleGame.score')} {score}
+                        </h2>
+                        {isCorrect && (
+                            <span className="absolute top-0 right-0 transform translate-x-1 -translate-y-1 bg-green-500 text-white rounded-full w-6 h-6 flex items-center justify-center text-xs animate-bounce">
+                                +1
+                            </span>
+                        )}
+                    </div>
+                </div>
                 <div className="text-3xl font-semibold my-6">{currentWord.nativeWord}</div>
                 <div className="grid grid-cols-2 gap-4">
                     {options.map((option, index) => (
@@ -97,7 +126,7 @@ const Shuffle = () => {
                             key={index}
                             onClick={() => handleOptionClick(option)}
                             className={`
-              py-3 px-4 text-lg font-medium rounded-lg transition-colors duration-200
+              py-3 px-4 text-lg font-medium rounded-lg transition-colors duration-200 overflow-auto
               ${isCorrect === true && option === currentWord.targetWord
                                     ? 'bg-green-500 text-white'
                                     : isCorrect === false && option !== currentWord.targetWord
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -66,10 +66,22 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "bounce": {
+          '0%, 20%, 50%, 80%, 100%': {
+            transform: 'translateY(0)',
+          },
+          '40%': {
+            transform: 'translateY(-30px)',
+          },
+          '60%': {
+            transform: 'translateY(-30px)',
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "bounce": 'bounce 0.7s ease-out',
       },
     },
   },
