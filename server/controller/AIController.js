bed533070a0f898018372578efc937f7d5e4c63c
alpturan-dev <abdurrahmanalpturan13@gmail.com>
2024-10-29 13:02:02 +0300

    fix AI prompt

--- server/controller/AIController.js
+++ server/controller/AIController.js
@@ -26,7 +26,7 @@ export const importWordsFromImages = async (req, res) => {
             };
         }));
 
-        const prompt = `Analyze the given images to identify word pairs consisting of a word in ${sourceLanguage} and ${targetLanguage}. Return the results as a valid JSON array. Each object in the array should contain the following properties: nativeWord (for the word in ${sourceLanguage}), targetWord (for the word in ${targetLanguage}). Especially in arabic words don't make any change in words, return the seen words as is. Don't try to translate the words cause the words is already given in the images.`;
+        const prompt = `Analyze the given images to identify word pairs consisting of a word in ${sourceLanguage} and ${targetLanguage}. Return the results as a valid JSON array. Each object in the array should contain the following properties: nativeWord (for the word in ${sourceLanguage}), targetWord (for the word in ${targetLanguage}). If the word has both nativeWord and targetWord in the image then do not translate the word and do not try to find its target word, just use these two words. But if there is no target word in the image, then translate the native word to the target language and use that as the target word. Especially in arabic words don't make any change in words, return the seen words as is. Don't try to translate the words if the words is already given in the images. If not possible to find the target word then translate and return it for targetWord.`;
 
         const result = await model.generateContent([prompt, ...imageParts]);
         const response = await result.response;
