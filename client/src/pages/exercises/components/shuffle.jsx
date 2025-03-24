7c98ec2a607331928162fcd5e770b54650855333
alpturan-dev <abdurrahmanalpturan13@gmail.com>
2025-03-24 18:57:27 +0300

    refactor: enhance Shuffle component layout and button styles

--- client/src/pages/exercises/components/shuffle.jsx
+++ client/src/pages/exercises/components/shuffle.jsx
@@ -1,7 +1,7 @@
 import { apiRequest } from "@/api/config";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
-import { ArrowLeft, Volume2 } from "lucide-react";
+import { ArrowLeft, RefreshCcw, Volume2 } from "lucide-react";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useTranslation } from "react-i18next";
@@ -104,7 +104,9 @@ const Shuffle = () => {
     if (selectedOption === currentWord.targetWord) {
       setIsCorrect(true);
       setScore(score + 1);
-      setTimeout(() => nextWord(words), 700);
+      setTimeout(() => {
+        nextWord(words);
+      }, 700);
     } else {
       setIsCorrect(false);
       setShowTryAgain(true);
@@ -172,7 +174,7 @@ const Shuffle = () => {
         <div className="flex items-center gap-2 text-2xl font-semibold text-gray-900 dark:text-white">
           <div className="flex gap-2 items-center">
             <Button size="sm" variant="outline" onClick={() => navigate(-1)}>
-              <ArrowLeft className="w-5 h-5" />
+              <ArrowLeft className="w-14 h-14" />
             </Button>
           </div>
           <div className="py-4 flex items-center gap-2 text-xl font-semibold">
@@ -180,37 +182,39 @@ const Shuffle = () => {
           </div>
         </div>
         <div className="pt-6 w-full flex justify-center">
-          <div className="relative w-1/3 md:w-1/6">
-            <div className="rounded-lg py-0.5 px-0.25 md:py-1 md:px-2 text-xl font-bold bg-primary text-primary-foreground">
+          <div className="relative w-full max-w-[200px] mx-auto">
+            <div className="rounded-lg py-2 px-4 text-xl font-extrabold bg-primary text-primary-foreground text-center shadow-sm">
               {t("shuffleExercise.score")} {score}
             </div>
             {isCorrect && (
-              <span className="absolute top-0 right-0 transform translate-x-1 -translate-y-1 bg-background text-white rounded-full w-6 h-6 flex items-center justify-center text-xs animate-bounce">
+              <span className="absolute -top-2 -right-2 bg-primary/80 text-white border-2 border-background rounded-full w-8 h-8 flex items-center justify-center text-sm font-bold animate-bounce shadow-sm">
                 +1
               </span>
             )}
           </div>
         </div>
-        <div className="flex items-center justify-center my-7">
+        <hr className="mt-4" />
+        <div className="flex items-center justify-center mt-6 mb-6">
           <Button
             variant="ghost"
-            className="py-0 pr-0 h-14 w-14"
+            className="rounded-full h-12 w-12 p-0 hover:bg-primary/70"
             onClick={() => playAudio(currentWord.nativeWord, languageCode)}
           >
-            <Volume2 className="mt-1.5" />
+            <Volume2 className="h-6 w-6" />
             <span className="sr-only">{t("common.playAudio")}</span>
           </Button>
-          <span className="text-3xl font-semibold ">
+          <span className="text-3xl font-bold tracking-tight">
             {currentWord.nativeWord}
           </span>
+          <span className="w-12 h-12"></span>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-x-14">
           {options.map((option, index) => (
             <button
               key={index}
               onClick={() => handleOptionClick(option)}
               className={`
-              py-1 px-2 md:py-3 md:px-4 text-lg font-medium rounded-lg transition-colors duration-200 overflow-auto
+              py-1 px-2 md:py-3 md:px-4 text-lg font-medium rounded-full transition-colors duration-200 overflow-auto
               ${
                 isCorrect === true && option === currentWord.targetWord
                   ? "bg-primary text-white"
@@ -218,7 +222,7 @@ const Shuffle = () => {
                   ? "bg-destructive text-white"
                   : isCorrect === false && option === currentWord.targetWord
                   ? "bg-primary hover:bg-primary/90 text-white"
-                  : "bg-gray-200 hover:bg-gray-300"
+                  : "bg-gray-200 hover:outline hover:outline-2 hover:outline-primary/50"
               }
               ${isCorrect !== null ? "cursor-not-allowed" : "cursor-pointer"}
             `}
@@ -238,6 +242,7 @@ const Shuffle = () => {
             onClick={handleTryAgain}
             className="mt-4 font-bold py-2 px-4 rounded"
           >
+            <RefreshCcw />
             {t("shuffleExercise.tryAgain")}
           </Button>
         )}
