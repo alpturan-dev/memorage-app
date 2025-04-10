985070ec11f68a5f406d2b2e44c0ecaeafd5a52f
alpturan-dev <abdurrahmanalpturan13@gmail.com>
2025-04-11 00:25:45 +0300

    refactor: enhance layout styling with font adjustments and improved color variables for consistency

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -5,7 +5,7 @@ import { scrollToTop } from "@/lib/utils";
 
 const Layout = () => {
   return (
-    <div className="bg-background">
+    <div className="bg-background font-sans">
       <Navbar />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col min-h-screen">
         <Outlet />
